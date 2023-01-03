3cbef0d34b41f2e5b2f06044ade273f4cff1aa2c
jeong-eun-park <p.j.eun555@gmail.com>
2023-01-03 11:01:42 +0900

    MODIFY: 1st initial setting

--- src/App.js
+++
@@ -1,7 +0,0 @@
-import "./App.css";
-
-function App() {
-  return <div className="App">adfsafs</div>;
-}
-
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+import Router from "./Router";
 import "./styles/reset.scss";
 import "./styles/common.scss";
 
