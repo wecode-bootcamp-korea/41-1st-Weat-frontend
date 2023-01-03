63f16fd508544912a49ba593b0c8cd136b997414
jeong-eun-park <p.j.eun555@gmail.com>
2023-01-03 18:05:04 +0900

    ADD: 로그인 레이아웃, 회원가입 레이아웃 추가

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
+import Nav2 from './components/nav/Nav';
+import Main from './pages/main/Main';
 import Category from './pages/Category/Category';
-import Detail from './pages/Category/Detail/Detail';
-import Cart from './pages/Cart/Cart';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
+import Detail from './pages/Category/detail/Detail';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 const Router = () => {
   return (
