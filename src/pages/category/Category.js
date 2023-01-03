59782ad5f5296daf9a871a535e686997e2491516
baegyungmin <bjy6439@naver.com>
2023-01-03 15:05:17 +0900

    Add: 카테고리페이지 완료

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,9 +1,56 @@
-import React from 'react'
+import React from 'react';
+import './Category.scss';
 
 const Category = () => {
   return (
-    <div>Category</div>
-  )
-}
+    <div className="category">
+      <div className="categoryImg">
+        <img src="/images/meat.jpg"></img>
+      </div>
+      <ul className="categoryBtn">
+        <li>돼지</li>
+        <li>소</li>
+        <li>닭</li>
+        <li>수산</li>
+        <li>밀키트</li>
+        <li>우유</li>
+        <li>달걀</li>
+        <li>이유식</li>
+      </ul>
+      <div className="categoryItemBox">
+        <ul className="categoryItem">
+          <li>
+            <img src="/images/meat.jpg"></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+          <li>
+            <img></img>
+            <button>a</button>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Category
+export default Category;
