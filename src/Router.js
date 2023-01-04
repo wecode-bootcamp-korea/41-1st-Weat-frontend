1eac8c6755853f1d5ec3606736e13949bae15e8f
baegyungmin <bjy6439@naver.com>
2023-01-04 18:03:33 +0900

    Add : CategoryPage 레이아웃 완료

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Main from './pages/Main/Main';
-import Category from './pages/Category/Category';
-import Detail from './pages/Category/Detail/Detail';
-import Cart from './pages/Cart/Cart';
-import Login from './pages/Login/Login';
-import Signup from './pages/Signup/Signup';
+import Main from './pages/main/Main';
+import Category from './pages/category/Category';
+import Detail from './pages/category/detail/Detail';
+import Cart from './pages/cart/Cart';
+import Login from './pages/login/Login';
+import Signup from './pages/signup/Signup';
 
 const Router = () => {
   return (
--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,9 +1,81 @@
-import React from 'react'
-
+import React from 'react';
+import { BsCart3 } from 'react-icons/bs';
+import { useNavigate } from 'react-router-dom';
+import './Category.scss';
 const Category = () => {
+  const navigate = useNavigate();
+  const MoveToDetail = () => {
+    navigate('/Category/Detail');
+  };
   return (
-    <div>Category</div>
-  )
-}
+    <div className="category">
+      <div className="categoryImg">
+        <img src="/images/meat.jpg" alt="#" />
+      </div>
+      <ul className="categoryBtn">
+        <li>돼지</li>
+        <li>소</li>
+        <li>닭</li>
+        <li>수산</li>
+        <li>밀키트</li>
+        <li>우유</li>
+        <li>달걀</li>
+        <li>이유식</li>
+      </ul>
 
-export default Category
+      <ul className="categoryItem">
+        <div onClick={MoveToDetail}>
+          <li>
+            <img src="/images/meat.jpg" alt="#" />
+            <button>
+              <BsCart3 />
+            </button>
+          </li>
+          <p>초신선 돼지 삼겹살 구이용</p>
+          <p>기준가 19,800원/600g</p>
+        </div>
+        <div onClick={MoveToDetail}>
+          <li>
+            <img src="/images/meat.jpg" alt="#" />
+            <button>
+              <BsCart3 />
+            </button>
+          </li>
+          <p>초신선 돼지 삼겹살 구이용</p>
+          <p>기준가 19,800원/600g</p>
+        </div>
+        <div onClick={MoveToDetail}>
+          <li>
+            <img src="/images/meat.jpg" alt="#" />
+            <button>
+              <BsCart3 />
+            </button>
+          </li>
+          <p>초신선 돼지 삼겹살 구이용</p>
+          <p>기준가 19,800원/600g</p>
+        </div>
+        <div onClick={MoveToDetail}>
+          <li>
+            <img src="/images/meat.jpg" alt="#" />
+            <button>
+              <BsCart3 />
+            </button>
+          </li>
+          <p>초신선 돼지 삼겹살 구이용</p>
+          <p>기준가 19,800원/600g</p>
+        </div>
+        <div onClick={MoveToDetail}>
+          <li>
+            <img src="/images/meat.jpg" alt="#" />
+            <button>
+              <BsCart3 />
+            </button>
+          </li>
+          <p>초신선 돼지 삼겹살 구이용</p>
+          <p>기준가 19,800원/600g</p>
+        </div>
+      </ul>
+    </div>
+  );
+};
+export default Category;
