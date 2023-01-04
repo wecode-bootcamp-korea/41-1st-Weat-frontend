6be25f22a05de22bf504f745d3b885c72836a95d
baegyungmin <bjy6439@naver.com>
2023-01-04 12:52:28 +0900

    Add : 상세페이지 레이아웃 완료

--- src/pages/category/detail/Detail.js
+++ src/pages/category/detail/Detail.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react';
+import './Detail.scss';
 
 const Detail = () => {
+  const [count, setCount] = useState(1);
+  const addCount = () => {
+    setCount(count + 1);
+  };
+  const DecCount = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    } else if (count <= 0) {
+      setCount(count);
+    }
+  };
   return (
-    <div>Detail</div>
-  )
-}
+    <div className="deatilPage">
+      <div className="detailTop">
+        <div className="detailImg" />
+        <div className="meatInfo">
+          <p className="meatName">초신선 돼지 삼겹살 구이용</p>
+          <p className="meatPrice">100g당 3000원</p>
+          <p className="meatPriceTotal">기준가 19,800원(600g)</p>
+          <div className="detailOptionType">
+            <span>옵션</span>
+            <div className="TypeCheckBox">보통</div>
+          </div>
+          <div className="meatCountBox">
+            <span>수량</span>
+            <div className="meatCount">
+              <button onClick={DecCount}>-</button>
+              <span>{count}</span>
+              <button onClick={addCount}>+</button>
+            </div>
+          </div>
+          <div />
+          <div className="oderBtn">
+            <button className="buyBtn">바로구매</button>
+            <button className="cartBtn">장바구니</button>
+          </div>
+        </div>
+      </div>
+      <div className="detailExplanation">
+        <div>상품리뷰</div>
+      </div>
+      <div />
+    </div>
+  );
+};
 
-export default Detail
+export default Detail;
