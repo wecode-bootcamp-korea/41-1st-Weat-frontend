b08dfe32f123217de6c5b640895612f583eeb69d
jeong-eun-park <p.j.eun555@gmail.com>
2023-01-05 09:51:51 +0900

    ADD: 통신준비중

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,7 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Signup.scss';
 
 const Signup = () => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: '',
+    confirmPassword: '',
+    userName: '',
+  });
+
+  const [phoneNumber, setPhoneNumeber] = useState('');
+
+  console.log(userData);
+  const handleChange = event => {
+    const { name, value } = event.target;
+    setUserData(prevData => {
+      return { ...prevData, [name]: value };
+    });
+  };
+
   return (
     <div className="signUp">
       <h1 className="title">회원가입</h1>
@@ -11,34 +28,63 @@ const Signup = () => {
         <div className="inputBox">
           <div className="inputLine">
             <div className="inputData">아이디(이메일주소)</div>
-            <input className="inputContent" type="text" />
+            <input
+              name="email"
+              value={userData.email}
+              onChange={handleChange}
+              className="inputContent"
+              type="text"
+            />
           </div>
           <div className="inputLine">
             <div className="inputData">비밀번호</div>
-            <input className="inputContent" type="password" />
+            <input
+              name="password"
+              value={userData.password}
+              onChange={handleChange}
+              className="inputContent"
+              type="password"
+            />
           </div>
           <div className="inputLine">
             <div className="inputData">비밀번호 확인</div>
-            <input className="inputContent" type="password" />
+            <input
+              name="confirmPassword"
+              value={userData.confirmPassword}
+              onChange={handleChange}
+              className="inputContent"
+              type="password"
+            />
           </div>
           <div className="inputLine">
             <div className="inputData">이름</div>
-            <input className="inputContent" type="text" />
+            <input
+              name="userName"
+              value={userData.userName}
+              onChange={handleChange}
+              className="inputContent"
+              type="text"
+            />
           </div>
           <div className="inputLine">
             <div className="inputData">휴대폰번호</div>
             <div className="inputNumber">
-              <select className="phoneNumber">
-                <option>010</option>
-                <option>011</option>
-                <option>016</option>
-                <option>017</option>
-                <option>019</option>
+              <select
+                name="firstNumber"
+                value={userData.phoneNumber.firstNumber}
+                onChange={handleChange}
+                className="phoneNumber"
+              >
+                <option value="010">010</option>
+                <option value="011">011</option>
+                <option value="016">016</option>
+                <option value="017">017</option>
+                <option value="019">019</option>
               </select>
               <span className="numberIcon"> - </span>
-              <input className="numberContent" type="text" />
+              <input className="numberContent" type="text" maxLength={4} />
               <span className="numberIcon"> - </span>
-              <input className="numberContent" type="text" />
+              <input className="numberContent" type="text" maxLength={4} />
             </div>
           </div>
         </div>
