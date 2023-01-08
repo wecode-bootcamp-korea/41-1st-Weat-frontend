358fd81a6862861ac3c036e08594f6de4e0c898a
jeong-eun-park <p.j.eun555@gmail.com>
2023-01-08 20:19:16 +0900

    MODIFY: 회원가입 수정중

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -13,7 +13,7 @@ const Signup = () => {
     lastNumber: '',
   });
 
-  const [emailError, setEailError] = useState(false);
+  const [emailError, setEmailError] = useState(false);
   const [nameError, setNameError] = useState(false);
   const [numberError, setNumberError] = useState(false);
 
@@ -24,7 +24,7 @@ const Signup = () => {
     });
   };
 
-  /*const checkEmail = e => {
+  /* const checkEmail = e => {
     fetch('', {
       method: '',
       headers: { 'Content-Type': 'application/json;charset=utf-8' },
@@ -32,24 +32,23 @@ const Signup = () => {
         email: userData.email,
       }),
     })
-      .then(res =>{
-        if(res.status === ){
-          setEmailError(false)
-        
-        } if(res.status === ){
-          setEmailError(true)
+      .then(res => {
+        if (res.ok) {
+          setEmailError(false);
+        } else {
+          setEmailError(true);
         }
       })
       .then(data => {});
   };
-  */
+*/
   const matchPassword = userData.password === userData.confirmPassword;
   const validEmail = !userData.email.includes('@') && userData.email.length > 0;
   const validName = userData.userName.length > 0;
   const validNumber =
     (userData.secondNumber.length && userData.lastNumber.length) === 4;
 
-  /*  const handleClick = () => {
+  const handleClick = () => {
     if (!emailError) {
       return;
     } else if (!matchPassword) {
@@ -71,14 +70,14 @@ const Signup = () => {
       }),
     })
       .then(res => {
-         if(res.status === ){
-          alert('회원가입을 축하합니다.')
-          navigate('/Login')
-        } 
+        if (res.ok) {
+          alert('회원가입을 축하합니다.');
+          navigate('/Login');
+        }
       })
       .then(data => {});
   };
-*/
+
   const navigate = useNavigate();
   const goToLogin = () => {
     navigate('/Login');
@@ -106,13 +105,18 @@ const Signup = () => {
               ) : (
                 <p className="errorMessage">올바른 이메일양식을 사용하세요.</p>
               )}
-              {emailError ? (
-                <p className="errorMessage">중복된 아이디입니다. </p>
-              ) : (
-                <p className="errorMessage">사용가능한 아이디입니다.</p>
-              )}
+              {userData.email.length > 0 &&
+                (emailError ? (
+                  <p className="errorMessage">중복된 아이디입니다. </p>
+                ) : (
+                  <p className="errorMessage">사용가능한 아이디입니다.</p>
+                ))}
             </div>
-            <button onClick={checkEmail} className="checkBtn" type="button">
+            <button
+              //onClick={checkEmail}
+              className="checkBtn"
+              type="button"
+            >
               아이디 중복확인
             </button>
           </div>
@@ -208,11 +212,7 @@ const Signup = () => {
         <button onClick={goToLogin} className="goToLogin" type="button">
           로그인으로
         </button>
-        <button
-          // onClick={handleClick}
-          className="goToSignUp"
-          type="button"
-        >
+        <button onClick={handleClick} className="goToSignUp" type="button">
           가입하기
         </button>
       </div>
