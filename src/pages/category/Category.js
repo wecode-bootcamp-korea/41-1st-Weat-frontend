2176ff8945f385d06ded06cfb80c80f2d193f364
baegyungmin <bjy6439@naver.com>
2023-01-09 17:48:40 +0900

    Add : pagination 기능 추가

--- src/pages/category/Category.js
+++ src/pages/category/Category.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { BsCart3 } from 'react-icons/bs';
-import { Link } from 'react-router-dom';
+import { Link, useSearchParams } from 'react-router-dom';
 import './Category.scss';
 
 const CATEGORY = [
@@ -14,58 +14,83 @@ const CATEGORY = [
 
 const Category = () => {
   const [items, setItems] = useState([]);
-  const [names, setNames] = useState('돼지');
+  const [itemLength, setItemLength] = useState();
+  const [names, setNames] = useState(1);
+  const [searchParams, setSearchParams] = useSearchParams();
+  const category = searchParams.get('category');
+  const page = searchParams.get('page');
 
   useEffect(() => {
-    fetch('/data/dataCartegory.json')
+    fetch(`http://10.58.52.137:3000/products?${searchParams.toString()}`)
       .then(res => res.json())
-      .then(data => setItems(data));
-  }, []);
+      .then(({ productList, listLength }) => {
+        setItemLength(listLength);
+        setItems(productList);
+      });
+  }, [category, page]);
 
-  const categorySet = e => {
-    setNames(e.target.id);
+  const categorySet = id => {
+    setNames(id);
+    searchParams.set('category', id);
+    setSearchParams(searchParams);
   };
+
+  const movePage = pageNumber => {
+    searchParams.set('page', pageNumber);
+    setSearchParams(searchParams);
+  };
+
+  let num = Math.ceil(itemLength / 6);
 
   return (
     <div className="category">
       <img src="/images/grill.jpg" alt="메인이미지" />
       <ul className="categoryBtn">
-        {CATEGORY.map((i, key) => {
+        {CATEGORY.map(({ category_name, id }) => {
           return (
             <li
-              key={key}
-              id={i.category_name}
-              className={'btn' + (names === i.category_name ? 'active' : '')}
-              onClick={categorySet}
+              key={id}
+              id={id}
+              className={'btn' + (names === id ? 'active' : '')}
+              onClick={() => categorySet(id)}
             >
-              {i.category_name}
+              {category_name}
             </li>
           );
         })}
       </ul>
       <div className="itemList">
         <ul className="categoryItem">
-          {items.map(({ id, name, price, category }) => {
-            if (names === category) {
-              return (
-                <li key={id}>
-                  <div className="items">
-                    <Link to={`/Category/Detail/${id}`}>
-                      <img src="/images/meat.jpg" alt="고기사진" />
-                    </Link>
-                    <button>
-                      <BsCart3 />
-                    </button>
-                  </div>
-                  <div>
-                    <p className="itemName">{name}</p>
-                    <p className="itemPrice">{price}</p>
-                  </div>
-                </li>
-              );
-            }
+          {items.map(({ id, name, price, thumbnail_image }) => {
+            return (
+              <li key={id}>
+                <div className="items">
+                  <Link to={`/Category/Detail/${id}`}>
+                    <img src={thumbnail_image} alt="고기사진" />
+                  </Link>
+                  <button>
+                    <BsCart3 />
+                  </button>
+                </div>
+                <div>
+                  <p className="itemName">{name}</p>
+                  <p className="itemPrice">{price}</p>
+                </div>
+              </li>
+            );
           })}
         </ul>
+      </div>
+      <div>
+        {Array(num)
+          .fill()
+          .map(i => {
+            return (
+              <button onClick={() => movePage(i + 1)} key={i + 1}>
+                {i}
+              </button>
+            );
+          })}
       </div>
     </div>
   );
