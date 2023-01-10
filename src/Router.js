840334e872a4e91b93e59be3f13f89415dd10dcd
jeong-eun-park <p.j.eun555@gmail.com>
2023-01-10 10:03:28 +0900

    MODIFY

--- src/Router.js
+++ src/Router.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Category from './pages/category/Category';
 import Detail from './pages/category/detail/Detail';
