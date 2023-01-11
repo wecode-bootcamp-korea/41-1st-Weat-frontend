27bbbfa2e6e3ae148d6db1f0873f502081399e1a
jihwan <jhjhjh01000@gmail.com>
2023-01-11 17:02:00 +0900

    Add : 장바구니 카운팅 백통신 완료

--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -18,7 +18,7 @@ const Main = () => {
   useEffect(() => {
     fetch(`${API_BASE}/products/`)
       .then(result => result.json())
-      .then(data => setItemList(data));
+      .then(data => setItemList(data.productList));
   }, []);
 
   useEffect(() => {
