f21802c20a30f3a5af4406b5596d721950a6ed9d
황선용 <hwangseon-yong@Yong-MacBookPro.local>
2023-01-12 10:27:34 +0900

    Modify: 포인트 기능 수정 및 멘토 리뷰 수정 완료

--- src/pages/paymentfinal/PaymentFinal.js
+++ src/pages/paymentfinal/PaymentFinal.js
@@ -1,24 +1,55 @@
 import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import { API_BASE } from '../../apiData';
 import './PaymentFinal.scss';
 
+const mockData = {
+  deliveryObj: {
+    from_name: '박정은',
+    from_mobile: '010-1004-1004',
+    from_email: 'angel@wecode.com',
+    to_name: '배경민',
+    to_mobile: '010-1111-1111',
+    to_address: '선릉 2호점',
+    delivery_charge: '3500.00',
+  },
+  productOrderList: [
+    {
+      thumbnail:
+        'https://user-images.githubusercontent.com/53294075/210498031-5020d9b8-b847-46b6-8576-be70e06435c9.jpg',
+      productName: '돼지 삼겹살 구이용',
+      optionName: '보통(16mm)',
+      baseUnit: '600g',
+      price: '19800.00',
+      quantity: 1,
+    },
+  ],
+  point: '708570.00',
+};
+
+const shippingPrice = 3500;
+
 const PaymentFinal = () => {
-  const [cartData, setCartData] = useState([]);
+  const location = useLocation();
+  const orderId = location?.state?.orderId;
+
+  const [paymentData, setPaymentData] = useState(mockData);
   useEffect(() => {
-    fetch(`${API_BASE}/orders/id`)
+    if (!orderId) {
+      return;
+    }
+    fetch(`${API_BASE}/orders/${orderId}`)
       .then(result => result.json())
       .then(data => {
-        setCartData(data);
+        setPaymentData(data);
       });
-  }, []);
+  }, [orderId]);
 
   const totalPrice =
-    cartData.reduce((prev, cur) => {
+    paymentData.productOrderList.reduce((prev, cur) => {
       prev += cur.price * cur.quantity;
       return prev;
     }, 0) || 0;
-  const shippingPrice = 3500;
-
   return (
     <div className="paymentFinal">
       <h1 className="paymentFinalFinish">주문 완료 되었습니다.</h1>
@@ -35,15 +66,13 @@ const PaymentFinal = () => {
           </tr>
         </thead>
         <tbody>
-          {cartData.map((data, index) => (
+          {paymentData.productOrderList.map((data, index) => (
             <tr className="paymentFinalItems" key={index}>
-              <th className="paymentFinalItemsName">{data.productId}</th>
-              <th className="paymentFinalItemsOption">
-                {data.productOptionId}
-              </th>
+              <th className="paymentFinalItemsName">{data.productName}</th>
+              <th className="paymentFinalItemsOption">{data.optionName}</th>
               <th className="paymentFinalItemsCount">{data.quantity}</th>
               <th className="paymentFinalItemsUnit">
-                {(data.baseUnit * data.quantity).toLocaleString()}g
+                {data.baseUnit.toLocaleString()}
               </th>
               <th className="paymentFinalItemsPrice">
                 {(data.price * data.quantity).toLocaleString()}원
@@ -63,7 +92,9 @@ const PaymentFinal = () => {
         </div>
         <div className="itemPaymentPoint">
           <div className="itemPaymentPointLeft">차감 후 적립금</div>
-          <div className="itemPaymentPointRight">30,000P</div>
+          <div className="itemPaymentPointRight">
+            {(paymentData.point - totalPrice).toLocaleString()}P
+          </div>
         </div>
         <div className="itemPaymentShippingPrice">
           <div className="itemPaymentShippingPriceLeft">배송비</div>
